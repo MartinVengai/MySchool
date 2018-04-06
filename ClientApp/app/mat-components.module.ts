7209521ab784491646abee9fbce31d31aab978b8
Martin Vengai <martvee97@gmail.com>
2018-04-06 23:53:22 +0200

    add mat-progress-bar

--- ClientApp/app/mat-components.module.ts
+++ ClientApp/app/mat-components.module.ts
@@ -17,12 +17,14 @@ import { MatNativeDateModule } from "@angular/material/core";
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatTabsModule } from "@angular/material/tabs";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @NgModule({
   exports: [
+    MatProgressBarModule,
     MatDividerModule,
     MatNativeDateModule,
     MatCheckboxModule,
